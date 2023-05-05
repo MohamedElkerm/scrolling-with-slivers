2aeb2216dd08220bdf8e03895bec3f62b39846a5
mohamed elkerm <mohamedelkerm584@gmail.com>
2023-05-05 21:28:12 +0300

    make sliver app bar

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -11,11 +11,59 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-            'Slivers'
-        ),
+      body: CustomScrollView(
+        slivers: [
+           SliverAppBar(
+            backgroundColor: Color.fromRGBO(1, 23, 54, 1),
+            leading: const Icon(Icons.add),
+            actions: const [
+              Icon(Icons.exit_to_app),
+            ],
+
+            pinned: true,
+            floating: false,
+            snap: false,
+
+            expandedHeight: 250,
+            flexibleSpace: FlexibleSpaceBar(
+              background: Image.network('https://buddy.works/blog/thumbnails/flutter/flutter-cover.png',fit: BoxFit.cover,),
+              centerTitle: true,
+              title: const Text(
+                "Sliver App Bar",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 16.0,
+                ),
+              ), //Text
+            ),
+          ),
+
+
+          SliverList(
+            delegate: SliverChildListDelegate(
+              _buildList(40),
+            ),
+          ),
+        ],
       ),
     );
+  }
+
+  List<Widget> _buildList(int count) {
+    List<Widget> listItems = [];
+    for (int i = 0; i < count; i++) {
+      listItems.add(
+        Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Text(
+            'Sliver Item ${i.toString()}',
+            style: const TextStyle(
+              fontSize: 22.0,
+            ),
+          ),
+        ),
+      );
+    }
+    return listItems;
   }
 }
